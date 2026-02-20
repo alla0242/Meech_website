790fcc7d239b7d1c9f3945c2303e6c54d57502ab
mat <alla0242@algonquinlive.com>
2026-02-20 12:08:14 -0500

    more styling

--- src/app/componnents/banner.jsx
+++ src/app/componnents/banner.jsx
@@ -10,37 +10,18 @@ export default function Banner() {
           <div className="absolute inset-0 grid grid-rows-3 gap-0 z-0 h-full p-0 m-0" style={{ gridTemplateColumns: '18% 18% 21% 19% 23%' }}>
             <div id="grid-item-1" className="bg-red-500 h-full w-full">
             </div>
-            <div id="grid-item-2" className="bg-blue-500 h-10">
+            <div id="grid-item-2" className="bg-blue-500 h-full w-full">
             </div>
-            <div id="grid-item-3" className="bg-green-500 h-10">
+            <div id="grid-item-3" className="bg-green-500 h-full w-full">
             </div>
-            <div id="grid-item-4" className="bg-red-500 h-10">
+            <div id="grid-item-4" className="bg-red-500 h-full w-full">
             </div>
-            <div id="grid-item-5" className="bg-blue-500 h-10">
+            <div id="grid-item-5" className="bg-blue-500 h-full w-full">
             </div>
-            <div id="grid-item-6" className="bg-green-500 h-10 w-10">
-            </div>
-            <div id="grid-item-7" className="bg-red-500 h-10 w-10">
-            </div>
-            <div id="grid-item-8" className="bg-blue-500 h-10 w-10">
-            </div>
-            <div id="grid-item-9" className="bg-green-500 h-10 w-10">
-            </div>
-            <div id="grid-item-10" className="bg-red-500 h-10 w-10">
-            </div>
-            <div id="grid-item-11" className="bg-blue-500 h-10 w-10">
-            </div>
-            <div id="grid-item-12" className="bg-green-500 h-10 w-10">
-            </div>
-            <div id="grid-item-13" className="bg-green-500 h-10 w-10">
-            </div>
-            <div id="grid-item-14" className="bg-green-500 h-10 w-10">
-            </div>
-            <div id="grid-item-15" className="bg-green-500 h-10 w-10">
-            </div>
+            
           </div>
           {/* TV Overlay - positioned above the grid */}
-          <Image src="/tv_overlay.png" alt="banner" fill className="object-fit z-10 pt-10" />
+          <Image src="/tv_overlay.png" alt="banner" fill className="object-fit z-10" />
         </div>
     </section>
   );
