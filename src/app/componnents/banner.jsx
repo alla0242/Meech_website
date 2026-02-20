164b9bd6368a6fe1f7acbaf9565c3a43fafff0cb
mat <alla0242@algonquinlive.com>
2026-02-20 12:07:07 -0500

    grid col set for first row with %... maybe shit, maybe not

--- src/app/componnents/banner.jsx
+++ src/app/componnents/banner.jsx
@@ -7,16 +7,16 @@ export default function Banner() {
     <section className="w-screen aspect-[5/3] flex items-center justify-center">
         <div className="relative w-full aspect-[5/3] bg-purple-500 ">
           {/* Grid layer - positioned below the overlay */}
-          <div className="absolute inset-0 grid grid-cols-5 grid-rows-3 gap-4 z-0 h-full">
+          <div className="absolute inset-0 grid grid-rows-3 gap-0 z-0 h-full p-0 m-0" style={{ gridTemplateColumns: '18% 18% 21% 19% 23%' }}>
             <div id="grid-item-1" className="bg-red-500 h-full w-full">
             </div>
-            <div id="grid-item-2" className="bg-blue-500 h-10 w-10">
+            <div id="grid-item-2" className="bg-blue-500 h-10">
             </div>
-            <div id="grid-item-3" className="bg-green-500 h-10 w-10">
+            <div id="grid-item-3" className="bg-green-500 h-10">
             </div>
-            <div id="grid-item-4" className="bg-red-500 h-10 w-10">
+            <div id="grid-item-4" className="bg-red-500 h-10">
             </div>
-            <div id="grid-item-5" className="bg-blue-500 h-10 w-10">
+            <div id="grid-item-5" className="bg-blue-500 h-10">
             </div>
             <div id="grid-item-6" className="bg-green-500 h-10 w-10">
             </div>
@@ -40,7 +40,7 @@ export default function Banner() {
             </div>
           </div>
           {/* TV Overlay - positioned above the grid */}
-          <Image src="/tv_overlay.png" alt="banner" fill className="object-fit z-10" />
+          <Image src="/tv_overlay.png" alt="banner" fill className="object-fit z-10 pt-10" />
         </div>
     </section>
   );
